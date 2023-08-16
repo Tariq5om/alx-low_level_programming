2887817575f40f2ecf4ea89cc2dd08d37432f210
Tariq5om <tariq79253@gmail.com>
2023-08-15 23:39:16 -0700

    Add the o file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char arr[] = "_putchar\n";
+	int i = 0;
+
+	while (i < 10)
+	{
+		_putchar(arr[i]);
+		i++;
+	}
+	return (0);
+}
