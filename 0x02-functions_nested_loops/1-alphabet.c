e99cf251f58af887543dab9a23db367b2b692be6
Tariq5om <tariq79253@gmail.com>
2023-08-16 03:52:03 -0700

    Add the 1 file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,23 @@
 #include "main.h"
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
 
 /**
 * main - check code
 * Return: Always 0
+* print_alphabet is a function that print alphabet
 */
 int main(void)
 {
+	/*print_alphabet is a function*/
 	print_alphabet();
 	return (0);
 }
--- 0x02-functions_nested_loops/print_alphabet.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-/**
-* This function print the alphabet
-*/
-void print_alphabet(void)
-{
-	printf("abcdefghijklmnopqrstuvwxyz\n");
-}
