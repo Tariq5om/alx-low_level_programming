4b7cb62f955c387c5bc5c5edafdbce73b57beb4b
Tariq5om <tariq79253@gmail.com>
2023-08-19 13:10:42 -0700

    Add the 3 file

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _islower - check if charachter is low or not
+*
+* @c: the character to check
+* Return: 1 if low, 0 if otherside
+*/
+int _islower(int c)
+{
+	int n;
+
+	/*_islower - is a function*/
+	n = _islower(c);
+	if (n == 0)
+		return (0);
+	else
+		return (1);
+}
