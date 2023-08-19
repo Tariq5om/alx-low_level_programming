c38a692f8af82319b7c6f21e5faca1dd5f2cb81b
Tariq5om <tariq79253@gmail.com>
2023-08-19 06:22:01 -0700

    Add the 2 file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 /**
  *
 include all header files
@@ -5,3 +8,5 @@ include all header files
 
 int _putchar(char c);
 void print_alphabet(void);
+void print_alphabet_x10(void);
+#endif
