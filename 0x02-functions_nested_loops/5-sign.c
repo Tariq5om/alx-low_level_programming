c38d28b441de29efdf72689aadd0e6d1629f61f1
Tariq5om <tariq79253@gmail.com>
2023-08-20 01:11:56 -0700

    Fix

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,24 +3,24 @@
 /**
 * print_sign - print the sign of a number
 *
-* @n - the number would ckeck it's sign
+* @n: the number would ckeck it's sign
 * Return: 1 if positive, 0 if 0, -1 if negative
 */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return (1);
 		_putchar('+');
+		return (1);
 	}
 	else if (n == 0)
 	{
-		return (0);
 		_putchar('0');
+		return (0);
 	}
 	else
 	{
+		_putchar('0');
 		return (-1);
-		_putchar('-');
 	}
 }
