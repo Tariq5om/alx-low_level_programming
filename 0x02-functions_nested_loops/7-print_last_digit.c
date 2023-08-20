e58c5e1eea475579ca49d004066f2580c4219c7f
Tariq5om <tariq79253@gmail.com>
2023-08-20 06:56:45 -0700

    Add the 7 file

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* print_last_digit - is a function that give last digit
+*
+* @p: the parameter
+* Return: n which the last digit of p
+*/
+int print_last_digit(int p)
+{
+	int n;
+
+	n = (p % 10);
+	return (n);
+}
