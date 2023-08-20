f80f2f73a36dbbf3aca12f5088cfb2f0e63420ae
Tariq5om <tariq79253@gmail.com>
2023-08-20 07:24:45 -0700

    Fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,12 +4,9 @@
 * print_last_digit - is a function that give last digit
 *
 * @p: the parameter
-* Return: n which the last digit of p
+* Return: p % 10 which the last digit of p
 */
 int print_last_digit(int p)
 {
-	int n;
-
-	n = (p % 10);
-	return (n);
+	return (p % 10);
 }
