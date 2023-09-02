5d17ca4700885c7673655a4c3437e35352eecfaf
Tariq5om <tariq79253@gmail.com>
2023-09-02 12:09:32 -0700

    Add the 10 file

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * print_triangle - function that print a triangle
+ *
+ * @size: is the size of tge triangle
+ */
+void print_triangle(int size)
+{
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		int i, l_, lh;
+
+		for (i = 1; i <= size; i++)
+		{
+			for (l_ = 1; l_ <= (size - i); l_++)
+			{
+				_putchar(' ');
+			}
+			for (lh = 1; lh <= i; lh++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
