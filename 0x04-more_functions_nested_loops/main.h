8db9fc62db3409d1a18ccbc0c084fd37c5a1ae04
Tariq5om <tariq79253@gmail.com>
2023-09-02 00:35:39 -0700

    Fix

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
+void print_diagonal(int n);
+void print_square(int size);
+void print_line(int n);
 void print_most_numbers(void);
 void more_numbers(void);
 int _putchar(char c);
