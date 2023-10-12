1d9a755ca5ad2194582e21d653a4ed0eadb7e350
Tariq5om <tariq79253@gmail.com>
2023-10-12 04:29:17 +0300

    0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,20 +1,6 @@
 #include "function_pointers.h"
 
 /**
- * print_name_as_is - print name
- *
- * @name: The name
-*/
-void print_name_as_is(char *name)
-{
-	int i, j;
-
-	for (i = 0; i < name[i]; i++)
-		;
-	for (j = 0; j < i; j++)
-		_putchar(name[j]);
-}
-/**
  * print_name - call a function
  *
  * @name: the name
@@ -23,6 +9,5 @@ void print_name_as_is(char *name)
 */
 void print_name(char *name, void (*f)(char *))
 {
-	f = &print_name_as_is;
 	f(name);
 }
