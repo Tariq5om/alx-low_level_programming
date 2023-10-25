4b790f91c45fb349c4d5dda30da2f136ae043b00
Tariq5om <tariq79253@gmail.com>
2023-10-25 01:43:23 -0700

    Fix

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,7 +5,7 @@ int _putchar(char c);
 int Det_prime(int i, int j);
 void _puts_recursion(char *s);
 int _strlen_recursion(char *b);
-int _palindrome(char *s, int n, int m)
+int _palindrome(char *s, int n, int m);
 void _print_rev_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
