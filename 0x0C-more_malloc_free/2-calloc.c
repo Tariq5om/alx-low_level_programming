e8ee10a573fe1f54c0466ae06c1793a1617e43e1
Tariq5om <tariq79253@gmail.com>
2023-11-02 01:41:42 -0700

    2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,17 +9,15 @@
 */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *p;
 	unsigned int i;
 	unsigned char *s;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	p = malloc(nmemb * size);
-	if (p == NULL)
+	s = malloc(nmemb * size);
+	if (s == NULL)
 		return (NULL);
-	s = p;
 	for (i = 0; i < nmemb * size; i++)
 		s[i] = 0;
-	return (p);
+	return (s);
 }
--- 0x0C-more_malloc_free/2t-calloc.c
+++ 0x0C-more_malloc_free/2t-calloc.c
@@ -1,5 +1,31 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
+/**
+ * _calloc - Allocate memory
+ *
+ * @nmemb: The number of elements
+ * @size: The size of each element
+ * Return: void
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	/*void *p;*/
+	unsigned int i;
+	unsigned char *s;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	s = malloc(nmemb * size);
+	if (s == NULL)
+		return (NULL);
+	/*s = p;*/
+	for (i = 0; i < nmemb * size; i++)
+		s[i] = 0;
+	return (s);
+}
 
 /**
  * simple_print_buffer - prints buffer in hexa
@@ -10,56 +36,39 @@
  */
 void simple_print_buffer(char *buffer, unsigned int size)
 {
-    unsigned int i;
-
-    i = 0;
-    while (i < size)
-    {
-        if (i % 10)
-        {
-            printf(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", buffer[i]);
-        i++;
-    }
-    printf("\n");
-}
-/**
- * _calloc - Allocate memory
- *
- * @nmemb: The number of elements
- * @size: The size of each element
- * Return: void
-*/
-void *_calloc(unsigned int nmemb, unsigned int size)
-{
-	void *ptr;
 	unsigned int i;
-    unsigned char *p;
 
-	if (nmemb == 0 || size == 0)
-		return (NULL);
-	ptr = malloc(nmemb * size);
-	if (ptr == NULL)
-		return (NULL);
-    p = ptr;
-	for (i = 0; i < nmemb * size; i++)
-		p[i] = 0;
-	return (p);
+	i = 0;
+	while (i < size)
+	{
+		if (i % 10)
+		{
+			printf(" ");
+		}
+		if (!(i % 10) && i)
+		{
+			printf("\n");
+		}
+		printf("0x%02x", buffer[i]);
+		i++;
+	}
+	printf("\n");
 }
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-    char *a;
+	char *a;
 
-    a = _calloc(98, sizeof(char));
-    strcpy(a, "Best");
-    strcpy(a + 4, " School! :)\n");
-    a[97] = '!';
-    simple_print_buffer(a, 98);
-    free(a);
-    return (0);
+	a = _calloc(98, sizeof(char));
+	strcpy(a, "Best");
+	strcpy(a + 4, " School! :)\n");
+	a[97] = '!';
+	simple_print_buffer(a, 98);
+	free(a);
+	return (0);
 }
