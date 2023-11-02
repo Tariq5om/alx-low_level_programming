5a096496f0a5bdf95d1892244ed5f92d9eab65cc
Tariq5om <tariq79253@gmail.com>
2023-11-02 00:49:25 -0700

    1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,47 +1,47 @@
 #include "main.h"
 
 /**
- * _strlen - calculate the length of array
+ * _strlen - Calculate the length of the two  array
  *
- * @s: the string
- * Return: the length
+ * @s: One of the two string
+ * Return: Size
 */
 unsigned int _strlen(char *s)
 {
-	unsigned int i;
+	unsigned int j;
 
-	for (i = 0; *(s + i) != '\0'; i++)
+	for (j = 0; s[j] != '\0'; j++)
 		;
-	return (i);
+	return (j);
 }
 /**
- * string_nconcat - concatenates two strings
+ * string_nconcat -conect two strings
  *
- * @s1: the first string
- * @s2: the second string
- * @n: the n-th from the s2
- * Return: the desired string
+ * @s1: first one
+ * @s2: second one
+ * @n: the n-th of s2
+ * Return:  the string
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s;
-	unsigned int i, j, l1, l2;
+	unsigned int i, j, n1, n2;
 
 	if (s1 == NULL)
-		s1 = "";
+		s1 = '\0';
 	if (s2 == NULL)
-		s2 = "";
-	l1 = _strlen(s1);
-	l2 = _strlen(s2);
-	if (n >= l2)
-		n = l2;
-	s = malloc(sizeof(char) * (l1 + n + 1));
+		s2 = '\0';
+	n1 = _strlen(s1);
+	n2 = _strlen(s2);
+	if (n >= n2)
+		n = n2;
+	s = malloc(sizeof(char) * (n1 + n + 1));/*Reseve the right amount sp*/
 	if (s == NULL)
 		return (NULL);
-	for (i = 0; i < l1; i++)
+	for (i = 0; i < n1; i++)/*Fill the blanks*/
 		*(s + i) = *(s1 + i);
 	for (j = 0; j < n; j++, i++)
 		*(s + i) = *(s2 + j);
-	*(s + i) = '\0';
+	*(s + i) = '\0';/*Fill the last element*/
 	return (s);
 }
