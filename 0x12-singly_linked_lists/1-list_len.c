2ea9d8215fde20b88f3495109e94cac65bed1b11
Tariq5om <tariq79253@gmail.com>
2023-12-11 08:45:32 -0800

    Task1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,13 +1,17 @@
 #include "lists.h"
+
 /**
  * list_len - prints all the elements of a list_t list.
  *
  * @h: Pointer to struct
- * Return: The number of elements
+ * Return: The number of nodes
  */
 size_t list_len(const list_t *h)
 {
-	if (!h)
-		return (0);
-	return ((sizeof(list_t) / sizeof(h)) - 1);
+        list_t *p;
+        size_t i;
+        p = (list_t *)h;/*Because h is const*/
+        for (i = 0; p; i++, p = (p->next))
+		;
+        return (i);
 }
