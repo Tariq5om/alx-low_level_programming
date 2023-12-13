b4a2f649a01e7fdee326cd2165330a1084dac785
Tariq5om <tariq79253@gmail.com>
2023-12-13 06:33:43 -0800

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list_t list
+ *
+ * @head: The head of the linked list
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *p1, *p2;
+
+	for (p1 = *head; p1; p1 = p2)
+	{
+		p2 = p1->next;
+		free(p1);
+		p1 = p2;
+	}
+	*head = NULL;
+}
