9f06cb9e5147adffb8555d5b2bebe3a964e7aa59
Tariq5om <tariq79253@gmail.com>
2024-01-04 04:37:03 -0800

    Fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,16 +8,17 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-        unsigned int i;
-        unsigned char *s;
-        if (nmemb == 0 || size == 0)
-                return (NULL);
-        s = malloc(nmemb * size);
-        if (s == NULL)
-                return (NULL);
-        for (i = 0; i < nmemb * size; i++)
-                s[i] = 0;
-        return (s);
+	unsigned int i;
+	unsigned char *s;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	s = malloc(nmemb * size);
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb * size; i++)
+		s[i] = 0;
+	return (s);
 }
 /**
  * read_textfile - reads a text file and prints it
