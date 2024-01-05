3f686b6c575bd5561b21a546cd0e9f6ec5270e1d
Tariq5om <tariq79253@gmail.com>
2024-01-04 22:15:59 -0800

    Fix

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -13,6 +13,6 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int create_file(const char *filename, char *text_content);
 void eclose(int *fd);
 #define inf (O_WRONLY | O_CREAT | O_TRUNC | S_IRUSR | S_IWUSR)
-#define len (_strlen(text_content))
+#define len (_strlen(text_content) + 1)
 #define inf2 (O_WRONLY | O_CREAT | O_TRUNC, 0664)
 #endif /*end if*/
